f617a115fd32cc7d0670e3e81c3415a2804ea1ee
wkdgusdn3 <wkdgusdn3@gmail.com>
2016-10-03 03:50:58 +0900

    개인정보 관리 page 에서 비밀번호 입력 안할 시 처리

--- static/javascript/manage_info.js
+++ static/javascript/manage_info.js
@@ -10,23 +10,43 @@ function updateInfo() {
 		return null;
 	}
 
-	$.ajax({
-		url: "/manage_info/update",
-		type: "post",
-		dataType : "json",
-		data: {seq:seq, password:SHA256(password), birth:birth, sex:sex},
-		success: function(data) {
-			var json = JSON.parse(JSON.stringify(data));
-			if(json.status == "success") {
-				alert("회원정보 변경이 완료되었습니다.");
-				location.href='/';	
-			} else {
+	if(password == "") {	// 비밀번호 입력 o
+		$.ajax({
+			url: "/manage_info/update",
+			type: "post",
+			dataType : "json",
+			data: {seq:seq, birth:birth, sex:sex},
+			success: function(data) {
+				var json = JSON.parse(JSON.stringify(data));
+				if(json.status == "success") {
+					alert("회원정보 변경이 완료되었습니다.");
+					location.href='/';	
+				} else {
+					alert("회원정보 변경에 실패하였습니다.");	
+				}
+			},
+			error: function(data) {
 				alert("회원정보 변경에 실패하였습니다.");	
 			}
-		},
-		error: function(data) {
-			alert("회원정보 변경에 실패하였습니다.");	
-		}
-	});
-
+		});
+	} else {				// 비밀번호 입력 x
+		$.ajax({
+			url: "/manage_info/update",
+			type: "post",
+			dataType : "json",
+			data: {seq:seq, password:SHA256(password), birth:birth, sex:sex},
+			success: function(data) {
+				var json = JSON.parse(JSON.stringify(data));
+				if(json.status == "success") {
+					alert("회원정보 변경이 완료되었습니다.");
+					location.href='/';	
+				} else {
+					alert("회원정보 변경에 실패하였습니다.");	
+				}
+			},
+			error: function(data) {
+				alert("회원정보 변경에 실패하였습니다.");	
+			}
+		});
+	}
 }
